5a63f6ea8a8228320548f499d998a48c7104f63d
Nahuel Lorenzo <nahuelorenzo@gmail.com>
2022-12-29 17:36:29 -0300

    agrego validaciones en la compra

--- js/app.js
+++ js/app.js
@@ -1,33 +1,37 @@
 function menuPrincipal(){
 
     let opcion
-    let totCompra = 0
+    let totCuenta = 0
+    let compra = 0
     let totalTodas = 0
     let contCompra = 0
-    let cantProductos = 0
+    let cantUsoFuncion = 0
     let acumuladorDesc = 0
+    let totAPagar = 0
 
     alert("Bienvenido al almacen Lorenzo")
 
     do {
-        opcion = parseInt(prompt("Ingrese una opcion:\n 1-Realizar una compra\n 2-Cerrar una compra\n 3-Ver estadistias\n 4-Salir"))
+        opcion = parseInt(prompt("Ingrese una opcion:\n 1-Realizar una compra\n 2-Cerrar una cuenta\n 3-Ver estadistias\n 4-Salir"))
         
         switch (opcion) {
             case 1:
-                totCompra = totCompra + comprarProducto()
-                if (totCompra != 0){
+                compra = comprarProducto()
+                if (compra != 0){
+                    totCuenta = totCuenta + compra
                     contCompra = contCompra + 1
-                    cantProductos = cantProductos + 1
+                    cantUsoFuncion = cantUsoFuncion + 1
                 }
                 break
 
             case 2:
-                let totAPagar = totCompra * descuento(totCompra,cantProductos)
-                alert("El total a pagar es: " + totAPagar)
-                acumuladorDesc = acumuladorDesc + (totCompra - totAPagar)
+                totAPagar = totCuenta * descuento(totCuenta,cantUsoFuncion)
+                alert("El descuento que se le hizo es de: " + (totCuenta - totAPagar))
+                alert("El total a pagar es: " + totAPagar + ". La cuenta se cerro")
+                acumuladorDesc = acumuladorDesc + (totCuenta - totAPagar)
                 totalTodas = totalTodas + totAPagar
-                totCompra = 0
-                cantProductos = 0
+                totCuenta = 0
+                cantUsoFuncion = 0
                 break
 
             case 3:
@@ -80,12 +84,18 @@ function comprarProducto(){
 function totalProducto(precio){
     let cant = 0
     cant = parseInt(prompt("Ingrese la cantidad que desea comprar:"))
-    return precio*cant
+    if (cant > 0){
+        return precio*cant
+    }
+    else{
+        alert("La cantidad ingresada es incorrecta. Vuelva a intentarlo")
+        return 0
+    }
 }
 
 function descuento(total,productos){
     if (total > 1000 && productos >= 3){
-        alert("Por superar los $1000 y comprar 3 o mas productos distintos le corresponde un descuento del 20%")
+        alert("Por superar los $1000 y utilizar la funcion realizar una compra 3 o mas veces le corresponde un descuento del 20%")
         return 0.80
     }
     else{
@@ -94,7 +104,7 @@ function descuento(total,productos){
                 alert("Por superar los $1000 le corresponde un descuento del 10%")
             }
             else{
-                alert("Por comprar 3 o mas productos le corresponde un descuento del 10%")
+                alert("Por utilizar la funcion realizar una compra 3 o mas veces le corresponde un descuento del 10%")
             }
             return 0.9
         }
@@ -106,7 +116,7 @@ function descuento(total,productos){
 }
 
 function estadisticas(tot, contadorCompras, acumuDescuentos){
-    op = parseInt(prompt("Ingrese una opcion segun la estadistica que quiera ver\n 1-Total de todas las compras\n 2-Promedio gastado por compra\n 3- Cantidad de compras\n 4-Cantidad de dinero que no se recaudo por los descuentos"))
+    op = parseInt(prompt("Ingrese una opcion segun la estadistica que quiera ver\n 1-Total de todas las compras\n 2-Promedio gastado por compra\n 3-Cantidad de compras\n 4-Cantidad de dinero que no se recaudo por los descuentos\n 5-Promedio de la cantidad de dinero perdido por descuentos por compra"))
     switch (op) {
         case 1:
             alert("El total de todas las compras es: " + tot)
@@ -123,6 +133,10 @@ function estadisticas(tot, contadorCompras, acumuDescuentos){
         case 4:
             alert("La cantidad de dinero que no se recaudo por los descuentos es: " + acumuDescuentos)
             break
+
+        case 5:
+            alert("La cantidad de dinero perdido en promedio por cada compra debido a los descuentos es: " + acumuDescuentos/contadorCompras)
+            break;
 
         default:
             alert("Opcion incorrecta")
