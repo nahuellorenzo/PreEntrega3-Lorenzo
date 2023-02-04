16a95853f7f464dbfb90fd37b39441e84bcc9503
Nahuel Lorenzo <nahuelorenzo@gmail.com>
2023-02-04 03:56:24 -0300

    nuevos colores y cards mejoradas

--- js/app.js
+++ js/app.js
@@ -72,11 +72,11 @@ function agregarTabla() {
         tbody.innerHTML +=
             `
                 <tr class="filasItem">
-                    <th class="celdas" scope="row">${cont}</th>
-                    <td class="celdas"> ${item.producto.nombre} </td>
-                    <td class="celdas"> ${item.producto.precio} </td>
-                    <td class="celdas"> ${item.cantidad} </td>
-                    <td class="celdas elim">
+                    <th class="celdas text-center" scope="row">${cont}</th>
+                    <td class="celdas text-center"> ${item.producto.nombre} </td>
+                    <td class="celdas text-center"> ${item.producto.precio} </td>
+                    <td class="celdas text-center"> ${item.cantidad} </td>
+                    <td class="celdas elim text-center">
                         <button class="btn btn-danger" id="boton${item.producto.id}" data-id="${item.producto.id}"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
                         <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
                         <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
